b25ecfdc871d01819fd1a1f9c3fcb70729021e76
冯磊 <71683364+mefengl@users.noreply.github.com>
2022-03-09 11:29:32 +0800

    Create flomo-Helper.js

---
+++ flomo-Helper.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         flomo助手
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  memo变灰，用于防窥
+// @author       mefengl
+// @match        https://flomoapp.com/mine
+// @icon         https://www.google.com/s2/favicons?domain=flomoapp.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.addEventListener('load', function () {
+        const memos = document.body.getElementsByClassName('content');
+        for(let memo of memos) {
+            memo.style.opacity = 0.3;
+        }
+})
+
+})();
