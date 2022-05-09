25bd51109e83c283c9f6c425642dfc7e88f1a8c4
mefengl <71683364+mefengl@users.noreply.github.com>
2022-05-09 23:08:51 +0800

    zz

--- flomo-Helper.js
+++ flomo-Helper.js
@@ -1,22 +1,22 @@
 // ==UserScript==
 // @name         🕊️flomo助手
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  memo变灰，用于防窥
 // @author       mefengl
-// @match        https://flomoapp.com/mine
+// @match        https://flomoapp.com/mine*
 // @icon         https://www.google.com/s2/favicons?domain=flomoapp.com
 // @license MIT
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
     window.addEventListener('load', function () {
         const memos = document.body.getElementsByClassName('content');
-        for(let memo of memos) {
+        for (let memo of memos) {
             memo.style.opacity = 0.3;
         }
-})
+    })
 
 })();
